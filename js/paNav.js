b55bc55001b0b7ff0526e17907f8ac644331586e
Иван Далеченко <ivandalechenko@gmail.com>
2023-08-30 16:01:30 +0300

    added some for pa

---
+++ js/paNav.js
@@ -0,0 +1,3 @@
+const paNav = '<div class="paNav container"><a href="paOverview.html">Overview</a><a href="paDeposit.html" class="active">Deposit</a><a href="#">Withdraw</a><a href="#">Transaction</a><a href="#">Transfer</a><a href="#">Invest</a><a href="#">Affilate</a><a href="#">Settings</a></div>';
+
+document.getElementById('root').innerHTML = paNav + document.getElementById('root').innerHTML;  
