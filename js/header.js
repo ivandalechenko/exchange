ba8695588716fe55695ec7f2e2d6ad9c1e2b6edc
Иван Далеченко <ivandalechenko@gmail.com>
2023-09-01 01:04:41 +0300

    added sliders on index

--- js/header.js
+++ js/header.js
@@ -1,4 +1,4 @@
-const header = '<header class="header"><div class="container header_container" id="header"><a href="/index.html" class="header_logo"><img src="img/general/logo.svg" alt="logo"></a><div class="header_nav">'
+const header = '<header class="header"><div class="container header_container" id="header"><a href="index.html" class="header_logo"><img src="img/general/logo.svg" alt="logo"></a><div class="header_nav">'
     + '<a href="paOverview.html">PA<img src="img/general/arrow_down.svg" alt="arrow_down"></a>'
     + '<a href="swap.html">Swap</a>'
     + '<a href="support.html">Support</a>'
@@ -6,6 +6,7 @@ const header = '<header class="header"><div class="container header_container" i
     + '<a href="ta.html">TA</a>'
     + '<a href="today.html">Today</a>'
     + '<a href="p2p.html">P2P</a>'
+    + '<a href="chart.html">Chart</a>'
     +
     '<a href="blog.html">Blog</a></div><div class="header_auth"><a href="login.html">Log in</a><a href="sign_up.html">Sign up</a></div><div class="header_burger"><button id="header_opener"><img src="img/general/burger.svg" alt=""></button></div></div></header>';
 document.getElementById('root').innerHTML = header + document.getElementById('root').innerHTML;  
